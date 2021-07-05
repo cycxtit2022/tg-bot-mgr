e66ac0df458cb60c4846332194b8a90b1e9c6d2e
zhengrong5600 <71885599+zhengrong5600@users.noreply.github.com>
2021-07-05 16:44:47 +0800

    由于网络或者我少上传等，问题所以可能会无法搭建，如果遇到问题可以联系我
    由于网络或我少上传文件等问题所以可能无法搭建，如果遇到问题可以联系我
    谢谢

---
+++ common/common.go
@@ -0,0 +1,34 @@
+package common
+
+import "encoding/json"
+
+// RuleMap 群组规则的字典，匹配规则=>回复内容
+type RuleMap map[string]string
+
+var (
+	// AllGroupRules 所有群组的规则字典
+	AllGroupRules = make(map[int64]RuleMap)
+	// AllGroupId 目前服务的所有群组的id
+	AllGroupId []int64
+)
+
+func (rm RuleMap) String() string {
+	s, err := json.Marshal(rm)
+	if err != nil {
+		return ""
+	}
+	return string(s)
+}
+
+// Json2kvs 将json字符串转化为规则字典
+func Json2kvs(rulesJson string) RuleMap {
+	tkvs := make(RuleMap)
+	_ = json.Unmarshal([]byte(rulesJson), &tkvs)
+	return tkvs
+}
+
+// AddNewGroup 在内存中添加新群组的条目
+func AddNewGroup(gid int64) {
+	AllGroupId = append(AllGroupId, gid)
+	AllGroupRules[gid] = make(RuleMap)
+}
---
+++ db/sqlite.go
@@ -0,0 +1,72 @@
+package db
+
+import (
+	"tg-keyword-reply-bot/common"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // 初始化gorm使用sqlite
+)
+
+var db *gorm.DB
+
+type setting struct {
+	gorm.Model
+	Key   string `gorm:"unique;not null"`
+	Value string
+}
+
+type rule struct {
+	gorm.Model
+	GroupId  int64 `gorm:"unique;not null"`
+	RuleJson string
+}
+
+// Init 数据库初始化，包括新建数据库（如果还没有建立），基本数据的读写
+func Init(newToken string) (token string) {
+	dbtmp, err := gorm.Open("sqlite3", "data.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db = dbtmp
+	db.AutoMigrate(&setting{}, &rule{})
+	var tokenSetting setting
+	db.Find(&tokenSetting, "Key=?", "token")
+	token = tokenSetting.Value
+	if newToken != "" {
+		token = newToken
+		if tokenSetting.ID > 0 {
+			tokenSetting.Value = newToken
+			db.Model(&tokenSetting).Update(tokenSetting)
+		} else {
+			db.Create(&setting{
+				Key:   "token",
+				Value: newToken,
+			})
+		}
+	}
+	readAllGroupRules()
+	return
+}
+
+// AddNewGroup 数据库中添加一条记录来记录新群组的规则
+func AddNewGroup(groupId int64) {
+	db.Create(&rule{
+		GroupId:  groupId,
+		RuleJson: "",
+	})
+}
+
+// UpdateGroupRule 更新群组的规则
+func UpdateGroupRule(groupId int64, ruleJson string) {
+	db.Model(&rule{}).Where("group_id=?", groupId).Update("rule_json", ruleJson)
+}
+
+func readAllGroupRules() {
+	var allGroupRules []rule
+	db.Find(&allGroupRules)
+	for _, rule := range allGroupRules {
+		ruleStruct := common.Json2kvs(rule.RuleJson)
+		common.AllGroupRules[rule.GroupId] = ruleStruct
+		common.AllGroupId = append(common.AllGroupId, rule.GroupId)
+	}
+}
